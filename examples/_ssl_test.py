3c723a75349d2e07b04c56a89b13378e6928eafc
Vitaly Markov <wild.desu@gmail.com>
2021-12-17 12:06:00 +0000

    Fix fix table names in ssl_test

--- examples/_ssl_test.py
+++ examples/_ssl_test.py
@@ -7,11 +7,11 @@ printer = pprint.PrettyPrinter(indent=4, width=140)
 with pyexasol.connect(dsn='exasol-test-database:8888', user='sys', password='exasol', encryption=True) as C:
 
     # Basic select
-    stmt = C.execute("SELECT * FROM users ORDER BY user_id LIMIT 5")
+    stmt = C.execute("SELECT * FROM exa_all_users LIMIT 5")
     printer.pprint(stmt.fetchall())
 
     # Basic HTTP transport
-    pd = C.export_to_pandas('users')
+    pd = C.export_to_pandas('exa_all_users')
     pd.info()
 
     # Dump certificate info
