b62d8b7eb5dcaf1b374b189fb22df3fc382a0777
Vitaly Markov <wild.desu@gmail.com>
2021-12-17 12:31:27 +0000

    Attempt to pull certificate from test container and use it during connection

--- examples/_ssl_test.py
+++ examples/_ssl_test.py
@@ -1,10 +1,13 @@
 import pyexasol
+import ssl
 
 import pprint
 printer = pprint.PrettyPrinter(indent=4, width=140)
 
 
-with pyexasol.connect(dsn='exasol-test-database:8888', user='sys', password='exasol', encryption=True) as C:
+with pyexasol.connect(dsn='exasol-test-database:8888', user='sys', password='exasol'
+                      , encryption=True
+                      , websocket_sslopt={'cert_reqs': ssl.CERT_REQUIRED, 'ca_certs': 'rootCA.crt'}) as C:
 
     # Basic select
     stmt = C.execute("SELECT * FROM exa_all_users LIMIT 5")
