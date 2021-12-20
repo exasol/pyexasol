f2dc59c2c7b7da40bba763f611dfee6a5615cb54
Vitaly Markov <wild.desu@gmail.com>
2021-12-20 12:47:47 +0000

    Update paths in SSL test

--- examples/_ssl_test.py
+++ examples/_ssl_test.py
@@ -7,7 +7,7 @@ printer = pprint.PrettyPrinter(indent=4, width=140)
 
 with pyexasol.connect(dsn='exasol-test-database:8888', user='sys', password='exasol'
                       , encryption=True
-                      , websocket_sslopt={'cert_reqs': ssl.CERT_REQUIRED, 'ca_certs': 'rootCA.crt'}) as C:
+                      , websocket_sslopt={'cert_reqs': ssl.CERT_REQUIRED, 'ca_certs': '/certificates/rootCA.crt'}) as C:
 
     # Basic select
     stmt = C.execute("SELECT * FROM exa_all_users LIMIT 5")
