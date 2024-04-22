7641c0021b828769260567697021e8bfaad234ae
Nicola Coretti <nicola.coretti@exasol.com>
2024-04-22 14:59:54 +0200

    Add examples to pull request ci

--- examples/_config.py
+++ examples/_config.py
@@ -3,4 +3,4 @@ import os
 dsn = os.environ.get('EXAHOST', 'localhost:8563')
 user = os.environ.get('EXAUID', 'SYS')
 password = os.environ.get('EXAPWD', 'exasol')
-schema = os.environ.get('EXASCHEMA', 'PYEXASOL_TEST')
+schema = os.environ.get('EXASCHEMA', 'TEST')
