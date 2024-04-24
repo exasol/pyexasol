8525e77cb1c9a873819af89f66c01d15036fe111
Nicola Coretti <nicola.coretti@exasol.com>
2024-04-24 13:39:52 +0200

    Setup smoke integration test

--- test/integration/integration_smoke_test.py
+++ test/integration/integration_smoke_test.py
@@ -1,2 +1,5 @@
-def test_smoke():
-    assert True
+def test_smoke(connection):
+    result = connection.execute("SELECT 1;")
+    expected = (1,)
+    actual = result.fetchall()[0]
+    assert expected == actual
