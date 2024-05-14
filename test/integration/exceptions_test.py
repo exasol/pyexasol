7bffc713bce0e1e6ca8e451fe7323bd7efa584cb
Nicola Coretti <nicola.coretti@exasol.com>
2024-05-14 15:44:43 +0200

    Add tests for exceptions

---
+++ test/integration/exceptions_test.py
@@ -0,0 +1,100 @@
+import pytest
+import pyexasol
+from pyexasol import (
+    ExaConnectionError,
+    ExaAuthError,
+    ExaQueryError,
+    ExaRequestError,
+    ExaRuntimeError,
+    ExaCommunicationError
+)
+
+
+@pytest.mark.exceptions
+def test_bad_dsn(connection):
+    with pytest.raises(ExaConnectionError):
+        pyexasol.connect(dsn="ThisIsBroken")
+
+
+@pytest.mark.exceptions
+@pytest.mark.parametrize("credentials", [
+    {"username": "wrongy"},
+    {"password": "no-tworking"}
+])
+def test_bad_credentails(credentials, dsn, user, password):
+    username = credentials.get("username", user)
+    password = credentials.get("password", password)
+    with pytest.raises(ExaAuthError):
+        pyexasol.connect(
+            dsn=dsn,
+            user=username,
+            password=password
+        )
+
+
+@pytest.mark.exceptions
+def test_invalid_sql(connection):
+    statement = "SELECT1;"
+    with pytest.raises(ExaQueryError):
+        connection.execute(statement)
+
+
+@pytest.mark.exceptions
+def test_read_from_closed_cursor(dsn, user, password, schema):
+    connection = pyexasol.connect(
+        dsn=dsn,
+        user=user,
+        password=password,
+        schema=schema,
+        fetch_size_bytes=1024
+    )
+    query = "SELECT * FROM users;"
+    cursor = connection.execute(query)
+    cursor.fetchone()
+    cursor.close()
+    with pytest.raises(ExaRequestError):
+        cursor.fetchall()
+
+
+@pytest.mark.exceptions
+def test_fetch_without_result_set(connection):
+    cursor = connection.execute("COMMIT;")
+    with pytest.raises(ExaRuntimeError):
+        cursor.fetchone()
+
+
+@pytest.mark.exceptions
+def test_duplicated_select_of_column(connection):
+    query = "SELECT user_name, user_name FROM USERS;"
+    with pytest.raises(ExaRuntimeError):
+        connection.execute(query)
+
+
+@pytest.mark.exceptions
+def test_attempt_to_Run_query_on_closed_connection(connection):
+    connection.close()
+    with pytest.raises(ExaRuntimeError):
+        connection.execute("SELECT 1;")
+
+
+@pytest.mark.exceptions
+def test_close_closed_connection(dsn, user, password, schema):
+    connections = [
+        pyexasol.connect(
+            dsn=dsn,
+            user=user,
+            password=password,
+            schema=schema,
+        ),
+        pyexasol.connect(
+            dsn=dsn,
+            user=user,
+            password=password,
+            schema=schema,
+        )
+    ]
+
+    connections[1].execute(f"KILL SESSION {connections[0].session_id()}")
+    with pytest.raises(ExaCommunicationError):
+        con = connections[0]
+        con.close()
