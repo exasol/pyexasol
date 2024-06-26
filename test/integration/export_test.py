035774e9208c5aeebf8f2491e6cdd2f613a382f0
Nicola Coretti <nicola.coretti@exasol.com>
2024-06-26 13:03:47 +0200

    Add basic test setup for ETL related functionality

---
+++ test/integration/export_test.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+@pytest.mark.etl
+def test_export_with_column_names():
+    assert False
+
+
+@pytest.mark.etl
+def test_skip_rows_in_export():
+    assert False
+
+
+@pytest.mark.etl
+def test_custom_export_callback():
+    assert False
+
+
+@pytest.mark.etl
+def test_export_with_reodered_columns():
+    assert False
+
+
+@pytest.mark.etl
+def test_export_with_custom_csv_format():
+    assert False
---
+++ test/integration/import_test.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.mark.etl
+def test_import_with_custom_csv_format():
+    assert False
+
+
+@pytest.mark.etl
+def test_import_with_reordered_columns():
+    assert False
+
+
+@pytest.mark.etl
+def test_custom_import_callback():
+    assert False
+
+
+@pytest.mark.etl
+def test_skip_rows_in_import():
+    assert False
