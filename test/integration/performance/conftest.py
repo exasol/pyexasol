ac4bb119b3dd0229fb4d4363ddf151242a4f5bec
Ariel Schulz <ariel.schulz@exasol.com>
2025-08-18 12:55:08 +0200

    Refactor and reduce tests to show critical differences & set up for usage with export

---
+++ test/integration/performance/conftest.py
@@ -0,0 +1,96 @@
+from pathlib import Path
+
+import pytest
+from integration.performance.helper import create_empty_table
+
+from pyexasol import ExaConnection
+
+
+class BenchmarkSpecifications:
+    def __init__(self):
+        self.initial_data_size: int = 1_000
+        self.target_data_size: int = 4_000_000
+        self.rounds: int = 5
+        # calculated fields - could move to a setter
+        iterations, data_each_round = self.calculate_iterations()
+        self.final_import_data_size: int = data_each_round * self.rounds
+        self.iterations: int = iterations
+
+    def calculate_iterations(self) -> tuple[int, int]:
+        """
+        Calculate how many times we need to multiply initial_data_size by 4
+        to reach or exceed target_data_size
+
+        Returns:
+            tuple: (number_of_iterations, final_value)
+        """
+        current_value = self.initial_data_size
+        iterations = 0
+        while current_value < self.target_data_size:
+            current_value *= 4
+            iterations += 1
+        return iterations, current_value
+
+
+@pytest.fixture(scope="session")
+def benchmark_specs():
+    return BenchmarkSpecifications()
+
+
+@pytest.fixture(scope="session")
+def tmp_source_directory(tmp_path_factory) -> Path:
+    return tmp_path_factory.mktemp("benchmark")
+
+
+@pytest.fixture(scope="session")
+def session_connection(connection_factory):
+    con = connection_factory()
+    yield con
+    con.close()
+
+
+@pytest.fixture(scope="session")
+def create_empty_sales_table(session_connection: ExaConnection) -> str:
+    table_name = "SALES"
+    create_empty_table(connection=session_connection, table_name=table_name)
+
+    yield table_name
+
+    ddl = f"DROP TABLE IF EXISTS {table_name};"
+    session_connection.execute(ddl)
+    session_connection.commit()
+
+
+@pytest.fixture(scope="session")
+def fill_sales_table(
+    session_connection: ExaConnection, create_empty_sales_table, benchmark_specs
+):
+    initial_query = f"""
+        INSERT INTO {create_empty_sales_table} (SALES_TIMESTAMP, PRICE, CUSTOMER_NAME)
+        SELECT ADD_SECONDS(TIMESTAMP'2024-01-01 00:00:00', FLOOR(RANDOM() * 365 * 24 * 60 * 60)),
+        RANDOM(1, 125),
+        SUBSTRING(
+        LPAD(TO_CHAR(FLOOR(RANDOM() * 1000000)), 6, '0'),
+        1,
+        FLOOR(RANDOM() * 3) + 5)
+        FROM dual
+        CONNECT BY level <= {benchmark_specs.initial_data_size}
+    """
+    session_connection.execute(initial_query)
+    session_connection.commit()
+
+    quadrupling_query = f"""
+    INSERT INTO {create_empty_sales_table} (SALES_TIMESTAMP, PRICE, CUSTOMER_NAME)
+        SELECT * FROM (
+            SELECT SALES_TIMESTAMP, PRICE, CUSTOMER_NAME FROM {create_empty_sales_table}
+            UNION ALL
+            SELECT SALES_TIMESTAMP, PRICE, CUSTOMER_NAME FROM {create_empty_sales_table}
+            UNION ALL
+            SELECT SALES_TIMESTAMP, PRICE, CUSTOMER_NAME FROM {create_empty_sales_table}
+        ) AS doubled_data;
+    """
+    for _ in range(benchmark_specs.iterations):
+        session_connection.execute(quadrupling_query)
+        session_connection.commit()
+
+    return create_empty_sales_table
---
+++ test/integration/performance/connection_http_transport_import_test.py
@@ -0,0 +1,106 @@
+from pathlib import Path
+
+import pandas as pd
+import polars as pl
+import pyarrow.csv as csv
+import pyarrow.parquet as pq
+import pytest
+from integration.performance.helper import create_empty_table
+
+from pyexasol import ExaConnection
+
+# ROUNDS = 5
+# ITERATIONS, FINAL_SIZE = calculate_iterations(
+#     start_value=INITIAL_SIZE, target_value=4_000_000
+# )
+
+
+@pytest.fixture
+def empty_import_into_table(connection: ExaConnection):
+    table_name = "TMP_SALES_COPY"
+    create_empty_table(connection=connection, table_name=table_name)
+
+    yield table_name
+
+    ddl = f"DROP TABLE IF EXISTS {table_name};"
+    connection.execute(ddl)
+    connection.commit()
+
+
+@pytest.fixture(scope="session")
+def create_csv(
+    session_connection: ExaConnection, tmp_source_directory: Path, fill_sales_table
+):
+    csv_path = tmp_source_directory / "test_data.csv"
+    session_connection.export_to_file(
+        csv_path,
+        f"SELECT SALES_TIMESTAMP, PRICE, CUSTOMER_NAME FROM {fill_sales_table}",
+    )
+    return csv_path
+
+
+@pytest.fixture(scope="session")
+def create_parquet(
+    session_connection: ExaConnection, tmp_source_directory: Path, create_csv
+):
+    parquet_path = tmp_source_directory / "test_data.parquet"
+
+    table = csv.read_csv(
+        create_csv,
+        read_options=csv.ReadOptions(
+            column_names=["SALES_TIMESTAMP", "PRICE", "CUSTOMER_NAME"],
+        ),
+    )
+    pq.write_table(table, parquet_path)
+
+    return parquet_path
+
+
+@pytest.fixture(scope="session")
+def create_pandas_dataframe(
+    session_connection: ExaConnection, tmp_source_directory: Path, create_csv
+):
+    return pd.read_csv(create_csv, header=None)
+
+
+@pytest.fixture(scope="session")
+def create_polars_dataframe(
+    session_connection: ExaConnection, tmp_source_directory: Path, create_csv
+):
+    return pl.read_csv(
+        create_csv, has_header=False, rechunk=False, infer_schema_length=10000
+    )
+
+
+@pytest.mark.parametrize(
+    "import_method, data_creator",
+    [
+        pytest.param("import_from_file", "create_csv", id="file"),
+        pytest.param("import_from_pandas", "create_pandas_dataframe", id="pandas"),
+        pytest.param("import_from_parquet", "create_parquet", id="parquet"),
+        pytest.param("import_from_polars", "create_polars_dataframe", id="polars"),
+    ],
+)
+def test_import_methods(
+    benchmark,
+    benchmark_specs,
+    connection: ExaConnection,
+    request,
+    empty_import_into_table,
+    import_method,
+    data_creator,
+):
+    data = request.getfixturevalue(data_creator)
+
+    def func_to_be_measured():
+        return getattr(connection, import_method)(
+            data,
+            table=empty_import_into_table,
+            import_params={"columns": ["SALES_TIMESTAMP", "PRICE", "CUSTOMER_NAME"]},
+        )
+
+    benchmark.pedantic(func_to_be_measured, iterations=1, rounds=benchmark_specs.rounds)
+
+    count_query = f"SELECT count(*) FROM {empty_import_into_table};"
+    count = connection.execute(count_query).fetchval()
+    assert count == benchmark_specs.final_import_data_size
