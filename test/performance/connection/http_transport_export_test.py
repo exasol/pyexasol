bbf6ee0773ddcd28a66781c432a64f45c9d460d6
Ariel Schulz <ariel.schulz@exasol.com>
2025-08-19 08:53:22 +0200

    Split into export to file and to memory so test complexity less

--- test/performance/connection/http_transport_export_test.py
+++ test/performance/connection/http_transport_export_test.py
@@ -28,19 +28,44 @@ def get_polars_dataframe_length(exported_polars: pl.DataFrame) -> int:
 
 
 @pytest.mark.parametrize(
+    "export_method, file_length_function",
+    [
+        pytest.param("export_to_list", get_list_length, id="list"),
+        pytest.param("export_to_pandas", get_pandas_dataframe_length, id="pandas"),
+        pytest.param("export_to_polars", get_polars_dataframe_length, id="polars"),
+    ],
+)
+def test_export_methods_to_memory(
+    benchmark,
+    benchmark_specs,
+    tmp_source_directory,
+    connection: ExaConnection,
+    fill_sales_table,
+    export_method: str,
+    file_length_function: Callable,
+):
+    def func_to_be_measured():
+        return getattr(connection, export_method)(
+            query_or_table=fill_sales_table,
+            query_params={"columns": ["SALES_TIMESTAMP", "PRICE", "CUSTOMER_NAME"]},
+        )
+
+    result = benchmark.pedantic(
+        func_to_be_measured, iterations=1, rounds=benchmark_specs.rounds
+    )
+
+    number_rows = file_length_function(result)
+
+    assert number_rows == benchmark_specs.final_export_data_size
+
+
+@pytest.mark.parametrize(
     "export_method, file_extension, file_length_function",
     [
         pytest.param("export_to_file", "csv", get_csv_length, id="file"),
-        pytest.param("export_to_list", None, get_list_length, id="list"),
-        pytest.param(
-            "export_to_pandas", None, get_pandas_dataframe_length, id="pandas"
-        ),
-        pytest.param(
-            "export_to_polars", None, get_polars_dataframe_length, id="polars"
-        ),
     ],
 )
-def test_export_methods(
+def test_export_methods_to_file(
     benchmark,
     benchmark_specs,
     tmp_source_directory,
@@ -50,26 +75,16 @@ def test_export_methods(
     file_extension: str,
     file_length_function: Callable,
 ):
-    export_dst = None
-    if file_extension:
-        export_dst = tmp_source_directory / f"test_data.{file_extension}"
+    export_dst = tmp_source_directory / f"test_data.{file_extension}"
 
     def func_to_be_measured():
-        kwargs = {
-            "query_or_table": fill_sales_table,
-            "query_params": {"columns": ["SALES_TIMESTAMP", "PRICE", "CUSTOMER_NAME"]},
-        }
-        if file_extension:
-            kwargs["dst"] = export_dst
-        return getattr(connection, export_method)(**kwargs)
+        return getattr(connection, export_method)(
+            dst=export_dst,
+            query_or_table=fill_sales_table,
+            query_params={"columns": ["SALES_TIMESTAMP", "PRICE", "CUSTOMER_NAME"]},
+        )
 
-    result = benchmark.pedantic(
-        func_to_be_measured, iterations=1, rounds=benchmark_specs.rounds
-    )
+    benchmark.pedantic(func_to_be_measured, iterations=1, rounds=benchmark_specs.rounds)
 
-    if file_extension:
-        number_rows = file_length_function(export_dst)
-    else:
-        number_rows = file_length_function(result)
-
+    number_rows = file_length_function(export_dst)
     assert number_rows == benchmark_specs.final_export_data_size
