73c6baa508f59b9e53e0238fa239f7841e1505a7
Ariel Schulz <ariel.schulz@exasol.com>
2025-09-17 09:57:24 +0200

    Simplify test structure per review comments

--- test/integration/import_and_export/conftest.py
+++ test/integration/import_and_export/conftest.py
@@ -1,7 +1,9 @@
 import copy
+import csv
 from dataclasses import dataclass
 from datetime import datetime
 from inspect import cleandoc
+from io import StringIO
 from pathlib import Path
 from typing import Optional
 
@@ -61,46 +63,57 @@ class DataSample:
     columns: list[str]
     list_dict: list[dict]
 
+    @staticmethod
+    def _convert_to_output_from_select(value):
+        if value is None:
+            return value
+        # a float needs to be converted to a string, like "SCORE"
+        if isinstance(value, float):
+            return f"{value}"
+        # a datetime needs to be converted to a formatted string, like "LAST_VISIT_TS"
+        if isinstance(value, datetime):
+            return value.strftime(DATETIME_STR_FORMAT)
+        return value
+
+    @staticmethod
+    def _convert_to_csv_format(value):
+        if value is None:
+            return value
+        # a bool needs to be converted to an integer
+        if isinstance(value, bool):
+            return int(value)
+        # a datetime needs to be converted to a formatted string & in additional quotes
+        if isinstance(value, datetime):
+            return f'"{value.strftime(DATETIME_STR_FORMAT)}"'
+        return value
+
     def list_tuple(self, selected_columns: Optional[list[str]] = None) -> list[tuple]:
         if selected_columns is None:
             selected_columns = self.columns
 
-        list_tuple = []
-        for row in self.list_dict:
-            inner_list = []
-            for key in selected_columns:
-                if row[key] is None:
-                    inner_list.append(row[key])
-                # float needs to be quoted
-                elif key == "SCORE":
-                    inner_list.append(f"{row[key]}")
-                elif key == "LAST_VISIT_TS":
-                    inner_list.append(row[key].strftime(DATETIME_STR_FORMAT))
-                else:
-                    inner_list.append(row[key])
-            list_tuple.append(tuple(inner_list))
-        return list_tuple
+        return [
+            tuple(
+                [
+                    self._convert_to_output_from_select(row[key])
+                    for key in selected_columns
+                ]
+            )
+            for row in self.list_dict
+        ]
 
     def csv_str(self, selected_columns: Optional[list[str]] = None) -> str:
         if selected_columns is None:
             selected_columns = self.columns
 
-        list_str = []
-        for row in self.list_dict:
-            inner_list = []
-            for key in selected_columns:
-                if row[key] is None:
-                    inner_list.append(f"{row[key]}")
-                # this is not ideal as the database & Python data use bool
-                elif key == "IS_GRADUATING":
-                    inner_list.append(f"{int(row[key])}")
-                # timestamp string needs special quotation in CSV
-                elif key == "LAST_VISIT_TS":
-                    inner_list.append(f'"{row[key]}"')
-                else:
-                    inner_list.append(str(row[key]))
-            list_str.append(",".join(inner_list))
-        return "\n".join(list_str) + "\n"
+        with StringIO() as output:
+            writer = csv.writer(
+                output, dialect="unix", quoting=csv.QUOTE_NONE, quotechar=None
+            )
+            for row in self.list_dict:
+                writer.writerow(
+                    [self._convert_to_csv_format(row[key]) for key in selected_columns]
+                )
+            return output.getvalue()
 
     def write_csv(
         self, directory: Path, selected_columns: Optional[list[str]] = None
