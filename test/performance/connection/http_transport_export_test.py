faef98f25f446243ec99af3f8f7ffa11c90f085b
Ariel Schulz <ariel.schulz@exasol.com>
2025-09-17 14:56:52 +0200

    Add performance tests for parquet cases & reduce runs to 5

--- test/performance/connection/http_transport_export_test.py
+++ test/performance/connection/http_transport_export_test.py
@@ -5,6 +5,7 @@ from typing import Callable
 import pandas as pd
 import polars as pl
 import pytest
+from pyarrow import dataset
 
 from pyexasol import ExaConnection
 
@@ -92,4 +93,140 @@ def test_export_methods_to_file(
     )
 
     number_rows = file_length_function(export_dst)
+    assert number_rows == benchmark_specs.final_data_size
+
+
+def test_export_to_parquet(
+    benchmark,
+    benchmark_specs,
+    tmp_path,
+    connection: ExaConnection,
+    fill_sales_table,
+):
+    def func_to_be_measured():
+        return connection.export_to_parquet(
+            dst=tmp_path,
+            query_or_table=fill_sales_table,
+            query_params={"columns": ["SALES_TIMESTAMP", "PRICE", "CUSTOMER_NAME"]},
+            callback_params={"existing_data_behavior": "overwrite_or_ignore"},
+        )
+
+    benchmark.pedantic(
+        func_to_be_measured,
+        iterations=1,
+        rounds=benchmark_specs.rounds,
+        warmup_rounds=benchmark_specs.warm_up_rounds,
+    )
+
+    parquet_dataset = dataset.dataset(tmp_path)
+    number_rows = sum(
+        row_group.num_rows
+        for fragment in parquet_dataset.get_fragments()
+        for row_group in fragment.row_groups
+    )
+    assert number_rows == benchmark_specs.final_data_size
+
+
+def test_export_to_parquet_without_parallel(
+    benchmark,
+    benchmark_specs,
+    tmp_path,
+    connection: ExaConnection,
+    fill_sales_table,
+):
+    def func_to_be_measured():
+        return connection.export_to_parquet(
+            dst=tmp_path,
+            query_or_table=fill_sales_table,
+            query_params={"columns": ["SALES_TIMESTAMP", "PRICE", "CUSTOMER_NAME"]},
+            callback_params={
+                "existing_data_behavior": "overwrite_or_ignore",
+                "use_threads": False,
+            },
+        )
+
+    benchmark.pedantic(
+        func_to_be_measured,
+        iterations=1,
+        rounds=benchmark_specs.rounds,
+        warmup_rounds=benchmark_specs.warm_up_rounds,
+    )
+
+    parquet_dataset = dataset.dataset(tmp_path)
+    number_rows = sum(
+        row_group.num_rows
+        for fragment in parquet_dataset.get_fragments()
+        for row_group in fragment.row_groups
+    )
+    assert number_rows == benchmark_specs.final_data_size
+
+
+def test_export_to_parquet_with_multiple_files_with_thread(
+    benchmark,
+    benchmark_specs,
+    tmp_path,
+    connection: ExaConnection,
+    fill_sales_table,
+):
+    def func_to_be_measured():
+        return connection.export_to_parquet(
+            dst=tmp_path,
+            query_or_table=fill_sales_table,
+            query_params={"columns": ["SALES_TIMESTAMP", "PRICE", "CUSTOMER_NAME"]},
+            callback_params={
+                "existing_data_behavior": "overwrite_or_ignore",
+                "max_rows_per_file": 1_000_000,
+                "max_rows_per_group": 5_000,
+            },
+        )
+
+    benchmark.pedantic(
+        func_to_be_measured,
+        iterations=1,
+        rounds=benchmark_specs.rounds,
+        warmup_rounds=benchmark_specs.warm_up_rounds,
+    )
+
+    parquet_dataset = dataset.dataset(tmp_path)
+    number_rows = sum(
+        row_group.num_rows
+        for fragment in parquet_dataset.get_fragments()
+        for row_group in fragment.row_groups
+    )
+    assert number_rows == benchmark_specs.final_data_size
+
+
+def test_export_to_parquet_with_multiple_files_without_thread(
+    benchmark,
+    benchmark_specs,
+    tmp_path,
+    connection: ExaConnection,
+    fill_sales_table,
+):
+    def func_to_be_measured():
+        return connection.export_to_parquet(
+            dst=tmp_path,
+            query_or_table=fill_sales_table,
+            query_params={"columns": ["SALES_TIMESTAMP", "PRICE", "CUSTOMER_NAME"]},
+            callback_params={
+                "existing_data_behavior": "overwrite_or_ignore",
+                "use_threads": False,
+                "max_rows_per_file": 1_000_000,
+                "max_rows_per_group": 5_000,
+            },
+        )
+
+    benchmark.pedantic(
+        func_to_be_measured,
+        iterations=1,
+        rounds=benchmark_specs.rounds,
+        warmup_rounds=benchmark_specs.warm_up_rounds,
+    )
+
+    parquet_dataset = dataset.dataset(tmp_path)
+    number_rows = sum(
+        row_group.num_rows
+        for fragment in parquet_dataset.get_fragments()
+        for row_group in fragment.row_groups
+    )
     assert number_rows == benchmark_specs.final_data_size
