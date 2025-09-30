fe67371235d112c827c7e1f328da6d32a6fe1d33
Ariel Schulz <43442541+ArBridgeman@users.noreply.github.com>
2025-09-30 13:16:15 +0200

    Feature/211 benchmark export and import (#253)
    * Modify db:start to accept parameters
    
    * Simplify workflows to use updated nox session db:start
    
    * Add CI workflow to run performance checks and upload artifacts
    
    * Add comment to explain why this value was initial chosen
    
    * Turn off performance tests in CI as checks not finalized to stable state

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
 import argparse
+import copy
+import glob
+import json
+import subprocess
 from pathlib import Path
 
 import nox
@@ -10,8 +14,13 @@ from exasol.toolbox.nox.tasks import *  # pylint: disable=wildcard-import disabl
 from nox import Session
 
 from noxconfig import (
+    DEFAULT_DB_VERSION,
     start_test_db,
     stop_test_db,
+)
+from scripts.benchmark import (
+    Benchmark,
+    CompareBenchmarks,
 )
 
 # default actions to be run if nothing is explicitly specified with the -s option
@@ -30,10 +39,41 @@ PREVIOUS_BENCHMARK = BENCHMARK_FILEPATH / "0001_performance.json"
 CURRENT_BENCHMARK = BENCHMARK_FILEPATH / "0002_performance.json"
 
 
+def _create_start_db_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="nox -s start:db",
+        usage="nox -s start:db -- [-h] [-t | --port {int} --db-version {str} --with-certificate]",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--port", default=8563, type=int, help="forward port for the Exasol DB"
+    )
+    parser.add_argument(
+        "--db-version",
+        default=DEFAULT_DB_VERSION,
+        type=str,
+        help="Exasol DB version to be used",
+    )
+    parser.add_argument(
+        "--with-certificate",
+        default=False,
+        action="store_true",
+        help="Add a certificate to the Exasol DB",
+    )
+    return parser
+
+
 @nox.session(name="db:start", python=False)
 def start_db(session: Session) -> None:
     """Start a test database"""
-    start_test_db(session=session)
+    parser = _create_start_db_parser()
+    args = parser.parse_args(session.posargs)
+    start_test_db(
+        session=session,
+        port=args.port,
+        db_version=args.db_version,
+        with_certificate=args.with_certificate,
+    )
 
 
 @nox.session(name="db:stop", python=False)
@@ -83,13 +123,62 @@ def run_examples(session: Session) -> None:
         session.error(1)
 
 
+def _get_name_from_path(line: str) -> str:
+    name = str(Path(line).name)
+
+    for old, new in {".py::": "__", "[": "_", "]": ""}.items():
+        name = name.replace(old, new)
+    return name
+
+
+@nox.session(name="performance:json", python=False)
+def performance_json(session: Session) -> None:
+    """Output JSON of performance tests for running in the CI."""
+    output = subprocess.run(
+        [
+            "pytest",
+            "--collect-only",
+            PERFORMANCE_TEST_DIRECTORY,
+            "-q",
+        ],
+        capture_output=True,
+        text=True,
+    )
+
+    if output.returncode != 0:
+        print(output)
+        session.error()
+
+    processed_output = [
+        {"path": line, "key": _get_name_from_path(line)}
+        for line in output.stdout.splitlines()
+        if PERFORMANCE_TEST_DIRECTORY.name in line
+    ]
+
+    config = {
+        "performance-tests": processed_output,
+        "python-version": ["3.12"],
+        "exasol-version": [DEFAULT_DB_VERSION],
+    }
+
+    print(json.dumps(config))
+
+
 def _create_performance_test_arg_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog="nox -s performance:test",
-        usage="nox -s performance:test -- [-h] [-t | test_path]",
+        usage="nox -s performance:test -- [-h] [--test_path {test_path} --file-name {file_name}]",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("test_path", help="Path to test file (can include its name)")
+    parser.add_argument(
+        "--test-path", dest="test_path", help="Path to test file (can include its name)"
+    )
+    parser.add_argument(
+        "--file-name",
+        dest="file_name",
+        help="Name under with to save the created benchmark file",
+        default=CURRENT_BENCHMARK.name,
+    )
     return parser
 
 
@@ -101,12 +190,107 @@ def performance_test(session: Session) -> None:
     """
     parser = _create_performance_test_arg_parser()
     args = parser.parse_args(session.posargs)
+    file_path = BENCHMARK_FILEPATH / args.file_name
 
     command = [
         "pytest",
         args.test_path,
         "--benchmark-sort=name",
-        f"--benchmark-json={CURRENT_BENCHMARK}",
+        f"--benchmark-json={file_path}",
     ]
-
     session.run(*command)
+
+
+def _create_performance_combine_arg_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="nox -s performance:combine",
+        usage="nox -s performance:test -- [-h] [--path {path}]",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--path",
+        dest="path",
+        help="Path specifier for files to combine; should include glob(s) ending with *.json",
+    )
+    return parser
+
+
+@nox.session(name="performance:combine", python=False)
+def performance_combine(session: Session) -> None:
+    """
+    Combine performance benchmarks from multiple JSON files to one
+    file.
+    """
+    parser = _create_performance_combine_arg_parser()
+    args = parser.parse_args(session.posargs)
+
+    matches = glob.glob(args.path)
+    files = sorted(
+        [filepath for i in matches if (filepath := Path(i)) and filepath.is_file()]
+    )
+
+    new_json_list = []
+    for file in files:
+        file_dict = json.loads(file.read_text())
+        for entry in file_dict["benchmarks"]:
+            entry_dict = copy.deepcopy(entry)
+            entry_dict["machine_info"] = copy.deepcopy(file_dict["machine_info"])
+            entry_dict["commit_info"] = copy.deepcopy(file_dict["commit_info"])
+            entry_dict["datetime"] = copy.deepcopy(file_dict["datetime"])
+            entry_dict["version"] = copy.deepcopy(file_dict["version"])
+            new_json_list.append(entry_dict)
+
+    new_json = json.dumps({"benchmarks": new_json_list}, indent=4)
+    CURRENT_BENCHMARK.write_text(new_json)
+
+
+@nox.session(name="performance:check", python=False)
+def performance_check(session: Session) -> None:
+    """Compare previous & current results of benchmarked performance tests.
+
+    An exception is raised if any of the following conditions are met:
+     - A test is present in only the previous or current results.
+     - A test is present in both results, but their medians differ by more than 5% in
+       either direction.
+
+    Note:
+      As tests are matched based on their full name, if a test is renamed or moved,
+      this would result in the names not being matched up properly and at least one
+      exception being raised.
+
+    In the event of reasonable changes, the benchmark file should be updated as
+    specified in the section `Updating the Benchmark JSON File` of the
+    ``doc/developer_guide``. If substantial changes are needed, like the condition
+    for comparing the tests is flakey, then many runs (> 15) should be done in the CI
+    to collect metrics to develop a new comparison threshold.
+
+    Note:
+      While it was not selected due to time-constraints and out-of-the-box performance,
+      it would be nice to use a non-parameterized test, like the
+      `Mann-Whitney U test <https://en.wikipedia.org/wiki/Mann%E2%80%93Whitney_U_test>`
+      to determine if the benchmarked results significantly deviated, as opposed to the
+      metric and threshold limit employed here.
+
+    The JSON files used for comparison are created using the ``pytest-benchmark``
+    package. While that package provides a CLI to compare benchmark files,
+    unfortunately, it does not indicate when a test is not present in the previous
+    benchmark data nor does it indicate if a test is running faster than before
+    (only slower).
+    """
+    previous_benchmark = Benchmark(PREVIOUS_BENCHMARK)
+    previous_benchmark.set_benchmark_data()
+
+    current_benchmark = Benchmark(CURRENT_BENCHMARK)
+    current_benchmark.set_benchmark_data()
+
+    compare_benchmarks = CompareBenchmarks(
+        previous_benchmark=previous_benchmark,
+        current_benchmark=current_benchmark,
+        # medians must be with 5% of one another
+        relative_median_threshold=0.05,
+    )
+    errors = compare_benchmarks.do_comparison()
+
+    if errors:
+        errors = ["\nthe comparison failed due to:"] + errors
+        session.error("\n".join(errors))
