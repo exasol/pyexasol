04a28149685000b62637dfc47a9f2cbd7af0718c
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-10-24 14:27:49 -0300

    Fixes formatting

--- pyexasol/callback.py
+++ pyexasol/callback.py
@@ -87,8 +87,9 @@ def export_to_parquet(pipe, dst: Union[Path, str], **kwargs) -> None:
     parse_options = csv.ParseOptions(newlines_in_values=True)
     read_options = csv.ReadOptions(use_threads=False)
     reader = csv.open_csv(pipe, read_options=read_options, parse_options=parse_options)
-    dataset.write_dataset(reader, base_dir=dst, use_threads=False, format="parquet", **kwargs)
-
+    dataset.write_dataset(
+        reader, base_dir=dst, use_threads=False, format="parquet", **kwargs
+    )
 
 
 def export_to_polars(pipe, dst, **kwargs) -> "polars.DataFrame":
