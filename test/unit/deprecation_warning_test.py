f60774a7d056fddfa747211cbe7c4cd300177dd2
Ariel Schulz <ariel.schulz@exasol.com>
2025-11-21 11:45:43 +0100

    Add ignore for unused import as intentially used here

--- test/unit/deprecation_warning_test.py
+++ test/unit/deprecation_warning_test.py
@@ -5,4 +5,4 @@ from pyexasol.warnings import PyexasolDeprecationWarning
 
 def test_import_db2_module_emits_deprecation_warning():
     with pytest.warns(PyexasolDeprecationWarning):
-        pass
+        from pyexasol import db2  # noqa: F401
