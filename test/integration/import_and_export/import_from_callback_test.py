637b60e5f69e5d7d7f9fac08f2547744b8135501
Ariel Schulz <ariel.schulz@exasol.com>
2026-03-09 11:30:03 +0100

    Add additional exception tests

--- test/integration/import_and_export/import_from_callback_test.py
+++ test/integration/import_and_export/import_from_callback_test.py
@@ -1,4 +1,5 @@
 import time
+from unittest.mock import patch
 
 import pytest
 from integration.import_and_export.helper import select_result
@@ -9,6 +10,13 @@ from pyexasol.exceptions import (
     ExaQueryError,
     ExaRuntimeError,
 )
+
+
+@pytest.fixture
+def input_filepath(tmp_path):
+    filepath = tmp_path / "input.csv"
+    filepath.touch()
+    return filepath
 
 
 @pytest.fixture
@@ -155,7 +163,7 @@ class TestImportFromCallbackExceptions:
 
         def import_cb_with_close(pipe, src, **kwargs):
             new_connection.close(disconnect=False)
-            time.sleep(1)
+            time.sleep(2)
             import_cb(pipe, src, **kwargs)
 
         with pytest.raises(ExaImportError) as ex:
@@ -164,3 +172,83 @@ class TestImportFromCallbackExceptions:
         assert len(ex.value.exceptions) == 2
         # race condition: the caught exception depends on how far the thread was
         assert type(ex.value.exceptions[1]) in (ExaCommunicationError, ExaRuntimeError)
+
+    @staticmethod
+    def test_http_thread_has_exception(
+        connection, input_filepath, empty_table, import_cb
+    ):
+        with patch("pyexasol.connection.ExaHttpThread.join_with_exc") as mock:
+            mock.side_effect = BrokenPipeError("Broken pipe in http_thread")
+
+            with pytest.raises(ExaImportError, match="2 sub-exceptions") as ex:
+                connection.import_from_callback(
+                    callback=import_cb,
+                    src=input_filepath,
+                    table=empty_table,
+                )
+
+        assert len(ex.value.exceptions) == 2
+        assert isinstance(ex.value.exceptions[0], BrokenPipeError)
+        assert isinstance(ex.value.exceptions[1], ExaQueryError)
+        assert (
+            "Following error occured while reading data"
+            in ex.value.exceptions[1].message
+        )
+
+    @staticmethod
+    def test_sql_thread_has_exception(connection, input_filepath, import_cb):
+        with pytest.raises(ExaImportError, match="1 sub-exception") as ex:
+            connection.import_from_callback(
+                callback=import_cb, src=input_filepath, table="DOES_NOT_EXIST"
+            )
+
+        assert len(ex.value.exceptions) == 1
+        assert isinstance(ex.value.exceptions[0], ExaQueryError)
+        assert "object DOES_NOT_EXIST not found" in ex.value.exceptions[0].message
+
+    @staticmethod
+    def test_abort_query(connection, input_filepath, empty_table, import_cb):
+        """
+        Due to a race condition, it's difficult to create a test with
+        connection.abort_query() that ensures that an exception would be raised.
+        Thus, we mock that here. Still, there is a race condition whether 1 or 2
+        exceptions are raised.
+        """
+        with patch("pyexasol.connection.ExaSQLImportThread.run_sql") as mock:
+            mock.side_effect = ExaQueryError(
+                message="Client requested execution abort.",
+                query="mock response",
+                connection=connection,
+                code="40007",
+            )
+
+            with pytest.raises(ExaImportError) as ex:
+                connection.import_from_callback(
+                    callback=import_cb,
+                    src=input_filepath,
+                    table=empty_table,
+                )
+
+        query_error_loc = 0
+        if len(ex.value.exceptions) == 2:
+            query_error_loc = 1
+
+        selected_exception = ex.value.exceptions[query_error_loc]
+        assert isinstance(selected_exception, ExaQueryError)
+        assert "Client requested execution abort." in selected_exception.message
+
+    @staticmethod
+    def test_export_callback_and_sql_have_different_exceptions(connection):
+        error = ValueError("Error from callback")
+
+        def import_cb(pipe, src, **kwargs):
+            raise error
+
+        with pytest.raises(ExaImportError) as ex:
+            connection.import_from_callback(
+                callback=import_cb, src=None, table="DOES_NOT_EXIST"
+            )
+
+        assert ex.value.exceptions[0] == error
+        assert isinstance(ex.value.exceptions[1], ExaQueryError)
+        assert len(ex.value.exceptions) == 2
