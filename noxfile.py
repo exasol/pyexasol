97dc434cd18fa9647cd40d14e94a559e81cbbb30
Ariel Schulz <ariel.schulz@exasol.com>
2026-06-23 09:09:45 +0200

    Switch to minimum_python_version

--- noxfile.py
+++ noxfile.py
@@ -16,7 +16,7 @@ from nox import Session
 from noxconfig import (
     DEFAULT_DB_VERSION,
     start_test_db,
-    stop_test_db,
+    stop_test_db, PROJECT_CONFIG,
 )
 from scripts.benchmark import (
     Benchmark,
@@ -157,7 +157,7 @@ def performance_json(session: Session) -> None:
 
     config = {
         "performance-tests": processed_output,
-        "python-version": ["3.12"],
+        "python-version": [PROJECT_CONFIG.minimum_python_version],
         "exasol-version": [DEFAULT_DB_VERSION],
     }
 
